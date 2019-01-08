72af892639fce8e025e127bd4da823a0d1584f28
Matthew Schroeter <schroeter.m83@gmail.com>
2019-01-07 21:25:01 -0600

    correct port for heroku

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -149,6 +149,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("'{}' is not a valid URI.".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
