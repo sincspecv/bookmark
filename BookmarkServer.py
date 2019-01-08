2707acd199594e531bf7170d864d63edf2f2aae7
Matthew Schroeter <schroeter.m83@gmail.com>
2019-01-07 21:37:33 -0600

    duh

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -157,5 +157,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
     server_address = ('', port)
-    httpd = http.server.ThreadHTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
